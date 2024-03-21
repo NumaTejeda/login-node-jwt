d3ea3f9b3e30d972776fc0776f43868da2a8793f
Nico <at.psicotrisquel@gmail.com>
2024-03-21 18:26:12 -0300

    Agrega authController.js y metodo de registro. Obtiene datos del form, aun no se hace nada con eso.

--- cotrollers/authController.js
+++ cotrollers/authController.js
@@ -1,2 +1,13 @@
 const jwt = require('jsonwebtoken'); //Para la autenticacion 
 const bcryptjs = require('bcryptjs'); //Para ecriptar la clave
+const conexion = require('../database/db') //Hacemos referencia a la conexion de la bd
+const { promisefy } = require('util'); //Indicamos que vamos a utlizar promesas, asyncronismo. 
+
+//metodo para registrarnos
+
+exports.register = async (req, res) => { //Cuando demos click en registrar el boton submit envia todo los datos del form al register que es un metodo no es la plantilla. 
+    const name = req.body.name;  //Capturando los datos
+    const user = req.body.user;
+    const pass = req.body.pass;
+    console.log(name + " " + user + " " + pass);
+}
--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+//Hacemos referencia al controlador para utilizar los metodos del controller
+const authController = require('../cotrollers/authController')
+
+// Router para las vistas
 router.get('/', (req, res) => {
     res.render('index')
 })
@@ -10,5 +14,10 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
     res.render('register')
 })
+
+
+
+//router para los metodos del controller 
+router.post('/register', authController.register)
 
 module.exports = router;
